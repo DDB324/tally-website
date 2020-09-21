2fd4e7728941816fdf8817bca726187a6aecf5e3
ddb324 <jiangdaoran@163.com>
2020-09-21 23:05:21 +0800

    deploy

---
+++ precache-manifest.71ef78d6235d6eaec94a24d5fff49f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26649fa43798a7636f9",
+    "url": "/tally-website/css/app.39aa587c.css"
+  },
+  {
+    "revision": "d398b76c43ea0fe17e24081155018676",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "b26649fa43798a7636f9",
+    "url": "/tally-website/js/app.68ae7cea.js"
+  },
+  {
+    "revision": "097bf9c62ea79c8b7838",
+    "url": "/tally-website/js/chunk-vendors.bf2929b7.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-website/robots.txt"
+  }
+]);
